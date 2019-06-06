24bc5e8021e82d6eb5702116328efd5d7f2c4de8
rajeshkumarramaswamy <rajeshkramaswamy@gmail.com>
2019-06-06 12:48:39 +0530

    Updated wsportal

--- website/js/config.js
+++ website/js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_8UUJelY2C', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1ll3ebnkqe1vtkac0p5lkkivr9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_NrMtSkkJH	', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '702h2shss56tal7oi13fcr0e9s', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://s4lxaz9v8d.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://nud87mlbv8.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
         email: 'rajesh.ramaswamy@netenrich.com'
