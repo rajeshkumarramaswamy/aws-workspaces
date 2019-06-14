b23c1533d5d81898fa516060506a00a4fb7e21c2
rajeshkumarramaswamy <rajeshkramaswamy@gmail.com>
2019-06-14 11:23:21 +0530

    Latest additions

--- website/js/config.js
+++ website/js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_xvYXS0MOm', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '65v2msh220nel414345c90nmk4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_UUwkaQ6FI', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4gvbkcgbrp6l9gjjun3o7udecr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://nud87mlbv8.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'http://bucketforworkspaces.s3-website-us-east-1.amazonaws.com' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
         email: 'rajesh.ramaswamy@netenrich.com'
