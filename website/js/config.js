8bc2bb0cc5247ddffb0d90d6371f7e32a3c83a06
rajeshkumarramaswamy <rajeshkramaswamy@gmail.com>
2019-06-19 14:12:11 +0530

    Updated user pool

--- website/js/config.js
+++ website/js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_UUwkaQ6FI', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4gvbkcgbrp6l9gjjun3o7udecr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_9sk2JEOFq', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4h656k7j8ido5oet5924sbbia6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://nud87mlbv8.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://5pe4oi9dme.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
         email: 'rajesh.ramaswamy@netenrich.com'
