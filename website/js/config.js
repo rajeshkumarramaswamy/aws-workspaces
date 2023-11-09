33c8d6d4dfc520800e0d932bef67bf558090570d
Karthick Gandhi <karthick.gandhi@teamviewer.com>
2023-11-09 10:55:15 +0100

    Approval parameters update

--- website/js/config.js
+++ website/js/config.js
@@ -1,13 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_9sk2JEOFq', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4h656k7j8ido5oet5924sbbia6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'eu-central-1_W5KTSaTEd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6dr01enpgi8n8mb3o3fgjo68qs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://5pe4oi9dme.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://m6kcyp2gx0.execute-api.eu-central-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
-        email: 'rajesh.ramaswamy@netenrich.com'
+        email: 'karthickgandhi@karthickgandhi.com'
     }
 };
